3c83b7ac74db8cdefc984eee46dff591504eb980
smy37 <seongminyang37@gmail.com>
2026-05-06 20:41:14 +0900

    20260506_rating !리뷰&

--- classify_by_day/al_20260506.py
+++ classify_by_day/al_20260506.py
@@ -2,23 +2,35 @@ from typing import List
 
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        v_to_idx = {}
-        for idx, v in enumerate(ratings):
-            if v not in v_to_idx:
-                v_to_idx[v] = []
-            v_to_idx[v].append(idx)
+        ## First Approach
+        # v_to_idx = {}
+        # for idx, v in enumerate(ratings):
+        #     if v not in v_to_idx:
+        #         v_to_idx[v] = []
+        #     v_to_idx[v].append(idx)
+        #
+        # candy = [1] * len(ratings)
+        #
+        # for v in list(sorted(v_to_idx.keys())):
+        #     for idx in v_to_idx[v]:
+        #         if idx - 1 >= 0:
+        #             if ratings[idx - 1] > v:
+        #                 candy[idx - 1] = max(candy[idx - 1], candy[idx] + 1)
+        #
+        #         if idx + 1 < len(ratings):
+        #             if ratings[idx + 1] > v:
+        #                 candy[idx + 1] = max(candy[idx] + 1, candy[idx + 1])
+        #
+        # return sum(candy)
 
-        candy = [1] * len(ratings)
+        candy_l = [1]*len(ratings)
+        for i in range(1, len(ratings)):
+            if ratings[i-1] < ratings[i]:
+                candy_l[i] = candy_l[i-1]+1
 
-        for v in list(sorted(v_to_idx.keys())):
-            for idx in v_to_idx[v]:
-                if idx - 1 >= 0:
-                    if ratings[idx - 1] > v:
-                        candy[idx - 1] = max(candy[idx - 1], candy[idx] + 1)
+        for i in range(len(ratings)-2, -1, -1):
+            if ratings[i] > ratings[i+1]:
+                candy_l[i] = max(candy_l[i], candy_l[i+1]+1)
 
-                if idx + 1 < len(ratings):
-                    if ratings[idx + 1] > v:
-                        candy[idx + 1] = max(candy[idx] + 1, candy[idx + 1])
-
-        return sum(candy)
+        return sum(candy_l)
 
