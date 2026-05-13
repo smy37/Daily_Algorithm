b100bc67bddb801b76813a832fcea6d1f6d2ab89
smy37 <seongminyang37@gmail.com>
2026-05-14 00:03:47 +0900

    20260513_여러조건이 들어간 백트래킹 !리뷰&

--- classify_by_day/al_20260513.py
+++ classify_by_day/al_20260513.py
@@ -7,14 +7,22 @@ class Solution:
         def dfs(cur_idx, dot_num, str_list):
             if dot_num == 3:
                 if len(s) - cur_idx <= 3:
-                    answer.append(".".join(str_list))
+
+                    str_list[-1] += s[cur_idx + 1:]
+                    flag = True
+                    for word in str_list:
+                        if len(word) != len(str(int(word))) or not (0 <= int(word) <= 255):
+                            flag = False
+                            break
+                    if flag:
+                        answer.append(".".join(str_list))
             if cur_idx == len(s) - 1 or len(str_list) >= 4 or dot_num >= 4:
                 return
             if len(str_list[-1]) == 3:
-                dfs(cur_idx + 1, dot_num + 1, str_list + [s[cur_idx]])
+                dfs(cur_idx + 1, dot_num + 1, str_list + [s[cur_idx + 1]])
             else:
 
-                dfs(cur_idx + 1, dot_num + 1, str_list + [s[cur_idx]])
+                dfs(cur_idx + 1, dot_num + 1, str_list + [s[cur_idx + 1]])
 
                 ori_num = int(str_list[-1])
                 str_list[-1] = str_list[-1] + s[cur_idx + 1]
