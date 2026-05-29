b6f1548da8963d4966be028784d35c3a7ab26f96
smy37 <69130114+smy37@users.noreply.github.com>
2026-05-29 16:18:41 +0900

    초안 bfs multi edge graph

--- classify_by_day/al_20260528.py
+++ classify_by_day/al_20260528.py
@@ -1,39 +1,38 @@
-def operation(num1, oper, num2):
-    if oper == '+':
-        return int(num1) + int(num2)
-    elif oper == '-':
-        return int(num1) - int(num2)
-
-record = {}
-def recur(array):
-    if len(array) == 3:         ## Case1에서의 끝단계
-        return operation(array[0], array[1], array[2])
-    else:
-        length = len(array)//2
-        score = []
-        for i in range(length):
-            temp_list = []
-            k = 2*i
-            if i == 0:
-                temp_list = [str(operation(array[k],array[k+1], array[k+2]))] + array[k+3:]
-            elif i == length -1:
-                temp_list = array[:k]+[str(operation(array[k], array[k + 1], array[k + 2]))]
-            else:
-                temp_list = array[:k] + [str(operation(array[k], array[k + 1], array[k + 2]))] + array[k+3:]
-            if tuple(temp_list) not in record:
-                t_score = recur(temp_list)
-                record[tuple(temp_list)] = t_score
-                score.append(t_score)
-            else:
-                score.append(record[tuple(temp_list)])
-        return max(score)
-
-def solution(array):
-    answer = 0
-    answer = recur(array)
-    return answer
+`from typing import List
+from collections import deque
 
 
-if __name__ == '__main__':
-    print(solution(["1","-","3","+","5","-","8"]), 1)
-    print(solution( ["5", "-", "10", "+", "1", "+", "2", "-", "4"]), -4)
+class Solution:
+    def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
+        graph = {}
+        for idx, nodes in enumerate(routes):
+            for i in range(len(nodes)):
+                for j in range(i+1, len(nodes)):
+                    a, b = nodes[i], nodes[j]
+                    if a not in graph:
+                        graph[a] = {}
+                    if b not in graph:
+                        graph[b] = {}
+
+                    if b not in graph[a]:
+                        graph[a][b] = set()
+
+                    if a not in graph[b]:
+                        graph[b][a] = set()
+
+                    graph[a][b].add(idx)
+                    graph[b][a].add(idx)
+
+        dq = deque()
+        visit = {source: True}
+        bus_list = set()
+
+
+
+
+if __name__ == "__main__":
+    r = [[1, 2, 7], [3, 6, 7]]
+    s = 1
+    t = 6
+    sol = Solution()
+    sol.numBusesToDestination(r, s, t)`
