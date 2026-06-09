15be2ce3f754f0b469e6c08458f6929d0ee0737b
smy37 <seongminyang37@gmail.com>
2026-06-09 23:19:03 +0900

    20260608_bst update !리뷰&

--- classify_by_day/al_20260608.py
+++ classify_by_day/al_20260608.py
@@ -5,48 +5,62 @@
 #         self.left = left
 #         self.right = right
 
+
 class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
-        def delete_node(cur, parent, b_first):
-            if b_first:
-                val = delete_node(cur.left, cur, False)
-                return val
-            else:
-                if not cur.right:
-                    if parent:
-                        parent.right = None
-                        return cur.val
-                    return None
-                else:
-                    val = delete_node(cur.right, cur, False)
-                    return val
-
-        def update_root(node):
-            if not node.left and not node.right:
-                return None
-            elif not node.left:
-                return node.right
-            elif not node.right:
-                return node.left
-            else:
-
-                left_val = delete_node(node, None, True)
-                node.val = left_val
-                return node
-
-        if root:
-            if not root.left and not root.right and key == root.val:
-                return None
-            cur = root
-            while cur:
-                if cur.val == key:
-                    update_root(cur)
-                    break
-                elif cur.val > key:
+        def update_graph(graph):
+            def delete_node(parent, cur):
+                while cur.left:
+                    parent = cur
                     cur = cur.left
-                elif cur.val < key:
-                    cur = cur.right
+
+                val = cur.val
+
+                if parent:
+                    parent.left = cur.right
+                else:
+                    graph.right = cur.right
+
+                return val
+
+            if not graph.left and not graph.right:
+                return None
+
+            elif not graph.left:
+                return graph.right
+
+            elif not graph.right:
+                return graph.left
+
+            else:
+                val = delete_node(None, graph.right)
+                graph.val = val
+                return graph
+
+        cur = root
+        parent = None
+
+        while cur:
+            if cur.val == key:
+                sub_graph = update_graph(cur)
+
+                if parent is None:
+                    return sub_graph
+
+                if parent.left == cur:
+                    parent.left = sub_graph
+
+                elif parent.right == cur:
+                    parent.right = sub_graph
+
+                return root
+
+            parent = cur
+
+            if cur.val > key:
+                cur = cur.left
+
+            elif cur.val < key:
+                cur = cur.right
 
         return root
-
-
