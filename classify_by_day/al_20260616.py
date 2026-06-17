d3da4edada55b4e82a3e4538aa1526516b011b0e
smy37 <69130114+smy37@users.noreply.github.com>
2026-06-17 23:10:06 +0900

    20260617_virtual trie !리뷰& !#&

--- classify_by_day/al_20260616.py
+++ classify_by_day/al_20260616.py
@@ -1,5 +1,27 @@
 class Solution:
-    def findKthNumber(self, n: int, k: int) -> str:
-        num_list = [str(i) for i in range(1, n+1)]
-        num_list.sort()
-        return num_list[k-1]
+    def findKthNumber(self, n: int, k: int) -> int:
+        def count_branch(prefix, n):
+            cur = prefix
+            next_prefix = prefix + 1
+            steps = 0
+
+            while cur <= n:
+                steps += min(n+1, next_prefix) - cur
+                cur *= 10
+                next_prefix *= 10
+            
+            return steps
+
+        cur = 1
+        k -= 1
+
+        while k > 0:
+            steps = count_branch(cur, n)
+            if steps <= k:
+                k -= steps
+                cur += 1
+            else:
+                k -= 1
+                cur *= 10
+
+        return cur
