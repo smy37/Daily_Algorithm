d8f9b0ae03ed0533325812538efd5234af50dfbd
smy37 <69130114+smy37@users.noreply.github.com>
2026-07-07 22:18:33 +0900

    20260704_string !리뷰&

--- classify_by_day/al_20260704.py
+++ classify_by_day/al_20260704.py
@@ -1,33 +1,19 @@
-num = 11
-test = [[0, 1, 5], [0, 3, 2], [0, 4, 3], [1, 4, 1], [3, 4, 10], [1, 2, 2], [2, 5, 3], [4, 5, 4]]
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        print(path.split("/"))
+        stack = []
 
-def solution(n, costs):
-    answer = 0
-    costs = sorted(costs, key = lambda x : x[2])
-    parent_node = {}
-    for i in range(n):
-        parent_node[i] = i
+        for temp in path.split("/"):
+            if len(temp) > 0:
+                if temp == ".":
+                    continue
+                elif temp == "..":
+                    if stack:
+                        stack.pop()
+                else:
+                    stack.append(temp)
+        
+        answer= "/".join(stack)
+        answer = "/" + answer
 
-    for i in costs:
-        max_v = max(i[0], i[1])
-        min_v = min(i[0], i[1])
-        if parent_node[min_v] < parent_node[max_v]:
-
-            answer += i[2]
-            cri = parent_node[max_v]
-            for j in parent_node:
-                if parent_node[j] == cri:
-                    parent_node[j] = parent_node[min_v]
-        elif parent_node[min_v] > parent_node[max_v]:
-
-            answer += i[2]
-            cri = parent_node[min_v]
-            for j in parent_node:
-                if parent_node[j] == cri:
-                    parent_node[j] = parent_node[max_v]
-
-    print(answer)
-
-    return answer
-
-solution(num, test)
+        return answer
