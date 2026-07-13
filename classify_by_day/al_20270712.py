b6abaf685a1381e76ed5581137f3b4e2700583a7
smy37 <69130114+smy37@users.noreply.github.com>
2026-07-13 23:39:30 +0900

    초안 수정

--- classify_by_day/al_20270712.py
+++ classify_by_day/al_20270712.py
@@ -1,26 +1,12 @@
-def solution(N, A):
-    answer = 0
-    temp = 0
-    flag = True
-
-    cri = 0
-    for i in range(len(A)-2):
-        cri = min(A[i], A[i+1], A[i+2])
-        if cri!= 0:
-            temp += cri
-            A[i] -= cri
-            A[i+1] -= cri
-            A[i+2] -= cri
-            flag = True
-    print("Step1: ", A, temp)
-    for i in range(len(A)-1):
-        cri = min(A[i], A[i+1])
-        if cri!= 0:
-            temp += cri
-            A[i] -= cri
-            A[i+1] -= cri
-    print("Step2: ", A, temp)
-    answer = sum(A) + temp
-    return answer
-
-print(solution(5, [1,4,2, 10, 4]))
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def recoverTree(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        
