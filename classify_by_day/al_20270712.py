7652ef02a83d312229ee4b17cb52059bb20d917d
smy37 <69130114+smy37@users.noreply.github.com>
2026-07-15 00:25:19 +0900

    20260712_spiral !리뷰&

--- classify_by_day/al_20270712.py
+++ classify_by_day/al_20270712.py
@@ -1,16 +1,25 @@
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
-        dx = [1,0,-1,0]
-        dy = [0,1,0,-1]
+        dy = [1,0,-1,0]
+        dx = [0,1,0,-1]
 
         board = [[0 for _ in range(n)] for _ in range(n)]
         board[0][0] = 1
         cur_x, cur_y = 0, 0
         cri = 0
-        for i in range(n*n-1):
-            cur_x += dx[cri]
-            cur_y += dy[cri]
-
-            board[cur_y][cur_x] = i+1
+        cnt = 0
+        while 1:
+            next_x = cur_x + dx[cri]
+            next_y = cur_y + dy[cri]
+            
+            if 0<= next_x < n and 0<=next_y<n and board[next_x][next_y] == 0:
+                board[next_x][next_y] = cnt+2
+                cur_x = next_x
+                cur_y = next_y
+                cnt += 1
+            else:
+                cri = (cri+1)%4
+            if cnt == n*n-1:
+                break
 
         return board
