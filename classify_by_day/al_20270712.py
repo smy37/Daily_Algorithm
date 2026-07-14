1e4fc572810b30c2dc03a96ddcce31515dd31561
smy37 <69130114+smy37@users.noreply.github.com>
2026-07-14 23:59:08 +0900

    20260712 수정

--- classify_by_day/al_20270712.py
+++ classify_by_day/al_20270712.py
@@ -1,12 +1,16 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def recoverTree(self, root: Optional[TreeNode]) -> None:
-        """
-        Do not return anything, modify root in-place instead.
-        """
-        
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        dx = [1,0,-1,0]
+        dy = [0,1,0,-1]
+
+        board = [[0 for _ in range(n)] for _ in range(n)]
+        board[0][0] = 1
+        cur_x, cur_y = 0, 0
+        cri = 0
+        for i in range(n*n-1):
+            cur_x += dx[cri]
+            cur_y += dy[cri]
+
+            board[cur_y][cur_x] = i+1
+
+        return board
