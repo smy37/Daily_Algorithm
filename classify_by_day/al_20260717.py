be29422674f5212cf89ece9827306e7cbcd54031
smy37 <69130114+smy37@users.noreply.github.com>
2026-07-18 23:12:57 +0900

    20260717_linked list !리뷰&
    Refactor solution function to partition linked list based on value x.

--- classify_by_day/al_20260717.py
+++ classify_by_day/al_20260717.py
@@ -1,43 +1,28 @@
-import sys
-import math
-import heapq
-from collections import deque
-def solution(jobs):
-    final = 0
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        num_list_left = []
+        num_list_right = []
 
-    hq = []
-    jobs = sorted(jobs)
-    dq = deque([[i[1], i] for i in jobs])
-    cur = 0
-
-    while len(dq) > 0:
-        temp = dq.popleft()
-
-        if temp[1][0] <= cur:
-            heapq.heappush(hq, temp)
-
-        else:
-            if len(hq) == 0:
-                cur = temp[1][0]
-                cur += temp[0]
-                final += temp[0]
-
+        cur = head
+        while cur:
+            if cur.val < x:
+                num_list_left.append(cur.val)
             else:
-                dq.appendleft(temp)
-                short_disk = heapq.heappop(hq)
-                final += cur - short_disk[1][0] + short_disk[0]
-                cur += short_disk[0]
+                num_list_right.append(cur.val)
 
+            cur = cur.next
 
-    if len(hq) == 0:
-        final = math.floor(final/len(jobs))
-    else:
-        while len(hq) > 0:
-            short_disk = heapq.heappop(hq)
-            final += cur - short_disk[1][0] + short_disk[0]
-            cur += short_disk[0]
-        final = math.floor(final/len(jobs))
-    return final
+        cur = None
 
-q = [[0, 3], [1, 9], [2, 6]]
-print(solution(q))
+        for i in range(len(num_list_right)-1, -1, -1):
+            cur = ListNode(num_list_right[i], cur)
+
+        for i in range(len(num_list_left)-1, -1, -1):
+            cur = ListNode(num_list_left[i], cur)
+
+        return cur
