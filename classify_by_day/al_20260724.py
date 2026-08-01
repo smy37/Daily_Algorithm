d3336eb409236c4c5aafba9fec419c21fdfde78c
smy37 <69130114+smy37@users.noreply.github.com>
2026-08-02 03:29:55 +0900

    20260724_index cal !리뷰&

--- classify_by_day/al_20260724.py
+++ classify_by_day/al_20260724.py
@@ -2,54 +2,23 @@ import copy
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        candidate = []
-
+        row = [set() for _ in range(9)]
+        col = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
         for i in range(9):
-            row = set()
-            col = set()
             for j in range(9):
                 if board[i][j] == ".":
-                    candidate.append((i, j))
-                
-                if board[i][j] != ".":
-                    if board[i][j] in row:
-                        return False
-                    row.add(board[i][j])
+                    continue
 
-                if board[j][i] != ".":
-                    if board[j][i] in col:
-                        return False
-                    col.add(board[j][i])
-                    
+                v = board[i][j]
 
+                b_idx = (i//3)*3+ j//3
+                if v in row[i] or v in col[j] or v in boxes[b_idx]:
+                    return False
 
-        def dfs(b, idx):
-            if idx == len(candidate):
-                return True
-            i, j = candidate[idx]
-            cri_x = (i//3)*3
-            cri_y = (j//3)*3
-            temp = {str(k) : True for k in range(1, 10)}
-            
-            for k in range(9):
-                if b[i][k] in temp:
-                    del temp[b[i][k]]
-                
-                if b[k][j] in temp:
-                    del temp[b[k][j]]
+                row[i].add(v)
+                col[j].add(v)
+                boxes[b_idx].add(v)
 
-                if b[cri_x+k//3][cri_y+k%3] in temp:
-                    del temp[b[cri_x+k//3][cri_y+k%3]]
-            
-            for k in temp:
-                b_copy = copy.deepcopy(b)
-                b_copy[i][j] = str(k)
-                flag = dfs(b_copy, idx+1)
-                if flag:
-                    return True
-
-            return False
-
-        return dfs(board, 0)
-
+        return True
                 
